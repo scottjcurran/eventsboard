84feab3bf2dcde98b70cc1ceb3bc92077bc5ccf0
Scott Curran <scurran@reneson.com>
2019-07-11 12:49:30 -0700

    add show events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,5 +4,26 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event created!"
+      redirect_to @event
+    else
+      flash.now[:alert] = "Event not created"
+      render "new"
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    @events = Event.all
+  end
+
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_date, :end_date, :venue, :location)
   end
 end
